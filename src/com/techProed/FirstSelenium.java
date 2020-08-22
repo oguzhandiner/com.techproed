5ee4345e4111349437f461906af47c51b786689c
Oguzhan <oguzhandiner061@gmail.com>
2020-08-22 17:45:15 +0300

    First Commit

---
+++ src/com/techProed/FirstSelenium.java
@@ -0,0 +1,13 @@
+package com.techProed;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSelenium {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:/Users/hp/Desktop/IT Kurs/Selenium Dependencies/Drivers/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("http:google.com");
+
+    }
+}
